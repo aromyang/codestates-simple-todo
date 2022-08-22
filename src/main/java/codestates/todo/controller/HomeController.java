ab9ba8a3d666b1da093a9e6b8610809b7eb18405
romcanrom <baros2san@gmail.com>
2022-08-22 12:01:30 +0900

    main commit

--- src/main/java/codestates/todo/controller/HomeController.java
+++ src/main/java/codestates/todo/controller/HomeController.java
@@ -1,6 +1,7 @@
 package codestates.todo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,11 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return "To-do Application !";
+    }
+
+    @GetMapping("/{nickName}")
+    public String welcome(@PathVariable String nickName) {
+        return "hello " + nickName;
     }
 
 }
